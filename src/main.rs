7e9c768f68b005ece9f15d50d6f067890bc92f84
ameyakore <kore.ameya@gmail.com>
2022-09-26 10:04:44 +0530

    use async_std try_join instead of pulling in another dependancy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use futures::try_join;
+use std::collections::HashMap;
+
+use async_std::prelude::FutureExt;
+use mangaverse_entity::models::genre::Genre;
 
 pub mod manganelo;
 pub mod readm;
@@ -8,8 +11,12 @@ async fn main() -> anyhow::Result<()>{
     println!("Hello, world!");
     let f1 = manganelo::entity::get_manganelo_genre();
     let f2 = readm::entity::get_readm_genre();
-    let mut r = try_join!(f1, f2)?;
+    let mut r = f1.try_join(f2).await?;
     r.0.extend(r.1);
-    println!("{:?}", r.0);
+    let all = r.0.into_iter().map(|f| (f.clone(), Genre {
+        id: String::new(),
+        name: f
+    })).collect::<HashMap<String, Genre>>();
+    println!("{:#?}", all);
     Ok(())
 }
